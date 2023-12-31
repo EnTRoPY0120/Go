e7545df839531aa5febacc87379d17ea43f920bb
EnTRoPY0120 <vijayarajdvr@gmail.com>
2023-12-31 22:54:05 +0530

    Added new routes to http-server

--- http-server/main.go
+++ http-server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -22,14 +23,36 @@ func sendJSONResponse(w http.ResponseWriter, response Person) {
 	w.Write(jsonResponse)
 }
 
-func fooHandler(w http.ResponseWriter, r *http.Request) {
+func createPerson(w http.ResponseWriter, r *http.Request) {
+	var person *Person
+
+	err := json.NewDecoder(r.Body).Decode(&person)
+	if err != nil {
+		panic(err)
+	}
+
+	sendJSONResponse(w, *person)
+}
+
+func getPerson(w http.ResponseWriter) {
 	person := Person{Name: "EnTRoPY"}
 
 	sendJSONResponse(w, person)
 }
 
+func personHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Println("GET")
+		getPerson(w)
+	case http.MethodPost:
+		fmt.Println("POST")
+		createPerson(w, r)
+	}
+}
+
 func main() {
-	http.Handle("/foo", http.HandlerFunc(fooHandler))
+	http.Handle("/foo", http.HandlerFunc(personHandler))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
